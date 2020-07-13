d27a0ba50b5e2df30be66045b5aa5aa9221bc4cc
Oleksii Sheiko <programmer.alex97@gmail.com>
2020-07-13 17:04:27 +0300

    Added a few comments

--- Multithreading_Python/multithreading_basic.py
+++ Multithreading_Python/multithreading_basic.py
@@ -1,24 +1,38 @@
 import threading
+# Python program to illustrate the concept
+# of threading
 
 
 def print_square(num):
+    """
+    function to print square of given num
+    """
     print(f'Square: {num * num}')
 
 
 def print_cube(num):
+    """
+    function to print cube of given num
+    """
     print(f'Cube: {num * num * num}')
 
 
 if __name__ == '__main__':
+    # creating thread
     t1 = threading.Thread(target=print_cube, args=(10,))
     t2 = threading.Thread(target=print_square, args=(10,))
 
+    # starting thread 1
     t1.start()
+    # starting thread 2
     t2.start()
 
+    # wait until thread 1 is completely executable
     t1.join()
+    # wait until thread 2 is completely executable
     t2.join()
 
+    # when both threads completely executed
     print('Done!')
 
 
