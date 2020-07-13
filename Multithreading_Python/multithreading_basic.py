665d268a74658c24bb056f8e3ab80fa624447629
Oleksii Sheiko <programmer.alex97@gmail.com>
2020-07-13 16:57:04 +0300

    The basic of multithreading in Python

---
+++ Multithreading_Python/multithreading_basic.py
@@ -0,0 +1,24 @@
+import threading
+
+
+def print_square(num):
+    print(f'Square: {num * num}')
+
+
+def print_cube(num):
+    print(f'Cube: {num * num * num}')
+
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=print_cube, args=(10,))
+    t2 = threading.Thread(target=print_square, args=(10,))
+
+    t1.start()
+    t2.start()
+
+    t1.join()
+    t2.join()
+
+    print('Done!')
+
+
