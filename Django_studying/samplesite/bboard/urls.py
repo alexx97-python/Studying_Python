66f7af1599f8ad641c4006fc3549b30f77bb14b9
Oleksii Sheiko <programmer.alex97@gmail.com>
2020-07-23 13:10:10 +0300

    Used DetailView from Django GenericViews

--- Django_studying/samplesite/bboard/urls.py
+++ Django_studying/samplesite/bboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import index, by_rubric, BbCreareView
+from .views import index, by_rubric, add_and_save, BbDetailView
 
 urlpatterns = [
-    path('add/',BbCreareView.as_view(), name='add'),
+    path('detail/<int:pk>/', BbDetailView.as_view(), name='detail'),
+    path('add/', add_and_save, name='add'),
     path('<int:rubric_id>/', by_rubric, name='by_rubric'),
     path('', index, name='index')
 ]
--- Django_studying/samplesite/bboard/views.py
+++ Django_studying/samplesite/bboard/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, reverse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
 
 from .models import Bb, Rubric
 from .forms import BbForm
 from django.urls import reverse_lazy
 
 
-class BbCreareView(CreateView):
+class BbDetailView(DetailView):
+    model = Bb
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
+
+
+
+class BbCreateView(CreateView):
     template_name = 'bboard/create.html'
     form_class = BbForm
     success_url = reverse_lazy('index')
@@ -33,3 +44,19 @@ def by_rubric(request, rubric_id):
     context = {'bbs': bbs, 'rubrics': rubrics,
                'current_rubric': current_rubric}
     return render(request, 'bboard/by_rubric.html', context)
+
+
+def add_and_save(request):
+    if request.method == 'POST':
+        bbf = BbForm(request.POST)
+        if bbf.is_valid():
+            bbf.save()
+            return HttpResponseRedirect(reverse('by_rubric',
+                                                kwargs={'rubric_id': bbf.cleaned_data['rubric'].pk}))
+        else:
+            context = {'form': bbf}
+            return render(request, 'bboard/create.html', context)
+    else:
+        bbf = BbForm()
+        context = {'form': bbf}
+        return render(request, 'bboard/create.html', context)
